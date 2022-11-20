8183482fc6be730293e722e80593b2cb186fe91c
Mr S Boshoff <22546510@su@cld-495549.cs.sun.ac.za>
2022-11-20 19:53:40 +0200

    Updates

---
+++ precache-manifest.3cb75f3c1a67822d3ae6cf0a388ff827.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb21b2e33dd057341bdf529bb0ac9373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f7676ad7a418be5f085",
+    "url": "/static/css/main.1b3d6cec.chunk.css"
+  },
+  {
+    "revision": "4806cf6561101653ec48",
+    "url": "/static/js/2.ef88cd2b.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.ef88cd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7676ad7a418be5f085",
+    "url": "/static/js/main.d4cc18ec.chunk.js"
+  },
+  {
+    "revision": "46851dcc51eb78b9b111",
+    "url": "/static/js/runtime-main.99510279.js"
+  },
+  {
+    "revision": "f34987fe49960fb8a772b1d48b8de2e4",
+    "url": "/static/media/avatar.f34987fe.png"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  },
+  {
+    "revision": "611f5674221673f6322ffbb38c0642d4",
+    "url": "/static/media/wallpaper.611f5674.png"
+  }
+]);
